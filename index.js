1037ee4fd47d649330b60bebfcdb26469e9e17e8
sapics <gv.nishino@gmail.com>
2017-04-03 18:31:10 +0900

    Fix to set ip address

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 var getClientIp = require('request-ip').getClientIp;
 var getGeo = require('geoip-country').lookup;
-
-function isPrivateNetwork(ip) {
-	//0.0.0.0 - 10.255.255.255, 172.16.0.0 - 172.31.255.255, 192.168.0.0 - 192.168.255.255, 127.0.0.1
-	const privateIpRegexp = /(192\.168\.[0-9]+\.[0-9]+|10\.[0-9]+\.[0-9]+\.[0-9]+|172\.(1[6-9]|2[0-9]|3[0-2])\.[0-9]+\.[0-9]+|127\.0\.0\.1)/;
-	return privateIpRegexp.test(ip);
-}
+// PRIVATE IP https://en.wikipedia.org/wiki/Private_network
+// 10.0.0.0 - 10.255.255.255, 172.16.0.0 - 172.31.255.255, 192.168.0.0 - 192.168.255.255, 127.0.0.1
+var privateIpReg = /^(?:192\.168\.[0-9]+\.[0-9]+|10\.[0-9]+\.[0-9]+\.[0-9]+|172\.(1[6-9]|2[0-9]|3[0-2])\.[0-9]+\.[0-9]+|127\.0\.0\.1)$/;
 
 function requestCountry(req, privateIpCountry) {
-	var ip = getClientIp(req);
-	if (privateIpCountry && isPrivateNetwork(ip)) {
-		return privateIpCountry;
-	} else {
-		var geo = getGeo();
-		return (geo && geo.country) || false;
-	}
+	var ip = getClientIp(req), geo = getGeo(ip);
+	return (geo && geo.country) || (privateIpCountry && privateIpReg.test(ip) && privateIpCountry);
 }
 
 requestCountry.middleware = function(options){
