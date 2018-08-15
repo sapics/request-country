1f7b977a1b161eebb226ec3fb18f950169a1d8d7
Rakshith Ravi <rakshithravi1997@gmail.com>
2018-08-15 07:29:12 +0530

    Added typescript definition file

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+import * as express from 'express';
+
+interface MiddlewareOptions {
+	attributeName: string,
+	privateIpCountry: string
+};
+
+declare function middleware(options: MiddlewareOptions) : express.RequestHandler;
+
+export default function requestCountry(req: express.Request, privateIpCountry: string) : string;
